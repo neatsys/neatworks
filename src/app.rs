5cb37f1613eb496ea9af2d6390bb35d570369639
sgdxbc <sung@comp.nus.edu.sg>
2024-02-23 11:32:56 +0000

    Add draft of new design of blob module

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,14 @@
 pub mod kvstore;
 
 pub trait App {
+    // following SpexPaxos convention, this interface takes "binary in, binary out" approach, avoids
+    // exposing type parameters (for `op` and `result`) to replication/transaction protocols, along
+    // with unnecessary ser/de overhead
+    //
+    // however, the binary format is not desirable for testing purpose. to mitigate this issue, the
+    // `impl App` that is for testing should works with utf-8 encoded human-readable (e.g. JSON)
+    // `op` and `result`. `rpc::Payload`, which comes with `Debug` impl that is optimized for this
+    // convention, should be used to wrap all `op` and `result` sites
     fn execute(&mut self, op: &[u8]) -> anyhow::Result<Vec<u8>>;
 }
 
--- src/blob.rs
+++ src/blob.rs
@@ -1,3 +1,36 @@
+// justification of dedicated blob transfer service
+//
+// the network messaging abstraction in this codebse i.e. `SendMessage` is
+// suitable for delivering short messages with negligible overhead (negligible
+// for delivering single message, overall networking overhead may still be
+// significant). when sending messages/data that is large enough to break this
+// expectation, use this module can provide more stable performance and
+// additional functions
+//
+// for applications deployed with udp-based transportation, a reliable delivery
+// service is demanded for any message that cannot be fit into several IP
+// segments, or it would be unpractical to deliver the message eventually
+// through resending. blob transfer service provides such reliability with
+// ephemeral TCP servers and connections, and it's safe to consider blob
+// transfer failures as fatal errors without hurting robustness
+//
+// although tcp-based deployment does not have the reliability concren above,
+// if the transportation delivers all messages destinating same remote address
+// sequentially (e.g. `net::tokio::Tcp`), blob messages may occupy the
+// transmission channel and postpone later messages longer than expect. also,
+// blob messages probably should be logged differently for diagnostic (e.g. we
+// probably don't want to log blob's full content). as the result dedicated
+// serivce (at least channels) for blob transfer is still desirable
+//
+// this blob service additionally supports cancellation on both sender and
+// receiver side, which can be helpful to improve bandwidth and performance
+// efficiency. in conclusion, for the following statements:
+// * protocol doesn't want to keep in mind that the sending may fail
+// * protocol can benefit from isolation between sending blob and ordinary
+//   messages
+// * protocol can make use of a cancellation interface
+// if any of these is true, a blob transfer service instance can be deployed
+
 use std::{
     fmt::Debug,
     net::{IpAddr, SocketAddr},
@@ -15,6 +48,82 @@ use crate::{
     event::SendEvent,
     net::{events::Recv, SendMessage},
 };
+
+pub mod exp {
+    use std::net::SocketAddr;
+
+    use tokio::sync::mpsc::UnboundedReceiver;
+    use tokio_util::sync::CancellationToken;
+
+    use crate::{event::SendEvent, net::SendMessage};
+
+    pub enum Event<A, M, N> {
+        Send {
+            addr: A,
+            message: M,
+            buf: Vec<u8>,
+            cancel: CancellationToken,
+        },
+        Accept {
+            serve_addr: SocketAddr,
+            into_recv_event: Box<dyn FnOnce(Vec<u8>) -> N + Send + Sync>,
+            cancel: CancellationToken,
+        },
+    }
+
+    pub struct Accept<M> {
+        pub inner: M,
+        serve_addr: SocketAddr,
+    }
+
+    pub trait Service<A, M, N> {
+        fn send(&mut self, dest: A, message: M, buf: Vec<u8>) -> anyhow::Result<CancellationToken>;
+
+        fn accept(
+            &mut self,
+            accept_event: &Accept<M>,
+            into_recv_event: impl FnOnce(Vec<u8>) -> N + Send + Sync + 'static,
+        ) -> anyhow::Result<CancellationToken>;
+    }
+
+    impl<T: SendEvent<Event<A, M, N>>, A, M, N> Service<A, M, N> for T {
+        fn send(&mut self, dest: A, message: M, buf: Vec<u8>) -> anyhow::Result<CancellationToken> {
+            let cancel = CancellationToken::new();
+            let send = Event::Send {
+                addr: dest,
+                message,
+                buf,
+                cancel: cancel.clone(),
+            };
+            SendEvent::send(self, send)?;
+            Ok(cancel)
+        }
+
+        fn accept(
+            &mut self,
+            accept_event: &Accept<M>,
+            into_recv_event: impl FnOnce(Vec<u8>) -> N + Send + Sync + 'static,
+        ) -> anyhow::Result<CancellationToken> {
+            let cancel = CancellationToken::new();
+            let accept_transfer = Event::Accept {
+                serve_addr: accept_event.serve_addr,
+                into_recv_event: Box::new(into_recv_event),
+                cancel: cancel.clone(),
+            };
+            SendEvent::send(self, accept_transfer)?;
+            Ok(cancel)
+        }
+    }
+
+    pub struct Serve<M>(M, SocketAddr);
+
+    pub fn session<A, M, N>(
+        events: UnboundedReceiver<Event<A, M, N>>,
+        net: Box<dyn SendMessage<A, Serve<M>> + Send + Sync>,
+    ) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
 
 #[derive(Clone)]
 pub struct Transfer<A, M>(pub A, pub M, pub Vec<u8>);
