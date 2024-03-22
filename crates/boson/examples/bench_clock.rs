4b17dbf19a1980724de0d186f77c1e5094f99606
sgdxbc <sung@comp.nus.edu.sg>
2024-03-22 01:55:19 +0000

    Update boson dependencies

--- crates/boson/examples/bench_clock.rs
+++ crates/boson/examples/bench_clock.rs
@@ -1,77 +1,72 @@
-use anyhow::Context as _;
+// use anyhow::Context as _;
 use boson::{index_secret, public_key, Clock};
 use plonky2::plonk::circuit_data::CircuitConfig;
-use plonky2_maybe_rayon::rayon;
+// use plonky2_maybe_rayon::rayon;
 use tracing::info;
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let config = CircuitConfig::standard_ecc_config();
 
-    rayon::ThreadPoolBuilder::new()
-        .build()
-        .context("Failed to build thread pool.")?
-        .install(|| {
-            info!(
-                "Using {} compute threads on {} cores",
-                rayon::current_num_threads(),
-                16
-            );
+    // info!(
+    //     "Using {} compute threads on {} cores",
+    //     rayon::current_num_threads(),
+    //     16
+    // );
 
-            const S: usize = 8;
-            let (clock, circuit) = Clock::<S>::genesis(
-                [(); S].map({
-                    let mut i = 0;
-                    move |()| {
-                        let secret = index_secret(i);
-                        i += 1;
-                        public_key(secret)
-                    }
-                }),
-                config,
-            )?;
+    const S: usize = 8;
+    let (clock, circuit) = Clock::<S>::genesis(
+        [(); S].map({
+            let mut i = 0;
+            move |()| {
+                let secret = index_secret(i);
+                i += 1;
+                public_key(secret)
+            }
+        }),
+        config,
+    )?;
+    clock.verify(&circuit)?;
+
+    // let clock10 = clock.increment(0, index_secret(0), &circuit)?;
+    // let clock11 = clock.increment(1, index_secret(1), &circuit)?;
+    // let clock2 = clock10.merge(&clock11, &circuit)?;
+
+    let mut clocks = Vec::new();
+    for index in 0..4 {
+        clocks.push(clock.clone());
+        // for i in 0..max_depth {
+        for i in 0..10 {
+            let clock =
+                clocks
+                    .last()
+                    .as_ref()
+                    .unwrap()
+                    .increment(index, index_secret(index), &circuit)?;
             clock.verify(&circuit)?;
-
-            // let clock10 = clock.increment(0, index_secret(0), &circuit)?;
-            // let clock11 = clock.increment(1, index_secret(1), &circuit)?;
-            // let clock2 = clock10.merge(&clock11, &circuit)?;
-
-            let mut clocks = Vec::new();
-            for index in 0..4 {
-                clocks.push(clock.clone());
-                // for i in 0..max_depth {
-                for i in 0..10 {
-                    let clock = clocks.last().as_ref().unwrap().increment(
-                        index,
-                        index_secret(index),
-                        &circuit,
-                    )?;
-                    clock.verify(&circuit)?;
-                    if index == 0 {
-                        info!(
-                            "proof length of depth {i} = {}",
-                            clock.proof.to_bytes().len()
-                        )
-                    }
-                    clocks.push(clock)
-                }
-            }
-            for _ in 0..32 {
-                use rand::seq::SliceRandom;
-                let clock1 = clocks.choose(&mut rand::thread_rng()).unwrap();
-                let clock2 = clocks.choose(&mut rand::thread_rng()).unwrap();
+            if index == 0 {
                 info!(
-                    "merge {:?} and {:?}",
-                    clock1.counters().collect::<Vec<_>>(),
-                    clock2.counters().collect::<Vec<_>>(),
-                );
-                let clock = clock1.merge(clock2, &circuit)?;
-                info!("merged into {:?}", clock.counters().collect::<Vec<_>>());
-                clock.verify(&circuit)?;
-                clocks.push(clock)
+                    "proof length of depth {i} = {}",
+                    clock.proof.to_bytes().len()
+                )
             }
-            anyhow::Result::<_>::Ok(())
-        })?;
+            clocks.push(clock)
+        }
+    }
+    for _ in 0..32 {
+        use rand::seq::SliceRandom;
+        let clock1 = clocks.choose(&mut rand::thread_rng()).unwrap();
+        let clock2 = clocks.choose(&mut rand::thread_rng()).unwrap();
+        info!(
+            "merge {:?} and {:?}",
+            clock1.counters().collect::<Vec<_>>(),
+            clock2.counters().collect::<Vec<_>>(),
+        );
+        let clock = clock1.merge(clock2, &circuit)?;
+        info!("merged into {:?}", clock.counters().collect::<Vec<_>>());
+        clock.verify(&circuit)?;
+        clocks.push(clock)
+    }
 
     Ok(())
 }
