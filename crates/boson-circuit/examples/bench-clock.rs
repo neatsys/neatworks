f83fee70b29ef09a59d42819a306119e1e4db453
sgdxbc <sung@comp.nus.edu.sg>
2024-04-21 02:55:24 +0000

    Check in circuit microbench results

--- crates/boson-circuit/examples/bench-clock.rs
+++ crates/boson-circuit/examples/bench-clock.rs
@@ -1,7 +1,14 @@
+use std::{
+    fmt::Write,
+    fs::write,
+    time::{Instant, SystemTime},
+};
+
 use boson_circuit::{index_secret, Clock};
 use plonky2::plonk::circuit_data::CircuitConfig;
-// use plonky2_maybe_rayon::rayon;
-use tracing::info;
+use plonky2_maybe_rayon::rayon;
+use rand::{thread_rng, Rng};
+// use tracing::info;
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
@@ -13,8 +20,9 @@ fn main() -> anyhow::Result<()> {
     //     rayon::current_num_threads(),
     //     16
     // );
+    let num_thread = rayon::current_num_threads();
 
-    const S: usize = 64;
+    const S: usize = 1 << 10;
     let (clock, circuit) = Clock::<S>::genesis(
         [(); S].map({
             let mut i = 0;
@@ -38,45 +46,63 @@ fn main() -> anyhow::Result<()> {
     // let clock2 = clock10.merge(&clock11, &circuit)?;
 
     let mut clocks = Vec::new();
-    for index in 0..4 {
-        clocks.push(clock.clone());
-        // for i in 0..max_depth {
-        for i in 0..10 {
-            let clock =
-                clocks
-                    .last()
-                    .as_ref()
-                    .unwrap()
-                    .increment(index, index_secret(index), &circuit)?;
-            clock.verify(&circuit)?;
-            let compressed = clock.proof.clone().compress(
-                &circuit.data.verifier_only.circuit_digest,
-                &circuit.data.common,
-            )?;
-            if index == 0 {
-                info!(
-                    "depth {i} proof size {} compressed {}",
-                    clock.proof.to_bytes().len(),
-                    compressed.to_bytes().len()
-                )
-            }
-            clocks.push(clock)
-        }
-    }
+    clocks.push(clock);
+    // for index in 0..4 {
+    //     clocks.push(clock.clone());
+    //     // for i in 0..max_depth {
+    //     for i in 0..10 {
+    //         let clock =
+    //             clocks
+    //                 .last()
+    //                 .as_ref()
+    //                 .unwrap()
+    //                 .increment(index, index_secret(index), &circuit)?;
+    //         clock.verify(&circuit)?;
+    //         let compressed = clock.proof.clone().compress(
+    //             &circuit.data.verifier_only.circuit_digest,
+    //             &circuit.data.common,
+    //         )?;
+    //         if index == 0 {
+    //             info!(
+    //                 "depth {i} proof size {} compressed {}",
+    //                 clock.proof.to_bytes().len(),
+    //                 compressed.to_bytes().len()
+    //             )
+    //         }
+    //         clocks.push(clock)
+    //     }
+    // }
+    let mut lines = String::new();
     for _ in 0..32 {
         use rand::seq::SliceRandom;
         let clock1 = clocks.choose(&mut rand::thread_rng()).unwrap();
         let clock2 = clocks.choose(&mut rand::thread_rng()).unwrap();
         // info!(
-        //     "merge {:?} and {:?}",
+        //     "with {:?} and {:?}",
         //     clock1.counters().collect::<Vec<_>>(),
         //     clock2.counters().collect::<Vec<_>>(),
         // );
-        let clock = clock1.merge(clock2, &circuit)?;
+        // let clock = clock1.merge(clock2, &circuit)?;
         // info!("merged into {:?}", clock.counters().collect::<Vec<_>>());
+        let index = thread_rng().gen_range(0..S);
+        let start = Instant::now();
+        let clock = clock1.update(index, index_secret(index), clock2, &circuit)?;
+        // info!("updated into {:?}", clock.counters().collect::<Vec<_>>());
+        writeln!(
+            &mut lines,
+            "{num_thread},{S},{}",
+            start.elapsed().as_secs_f32()
+        )?;
         clock.verify(&circuit)?;
         clocks.push(clock)
     }
+    write(
+        format!(
+            "../../tools/boson-control/notebooks/circuit-{}.txt",
+            SystemTime::UNIX_EPOCH.elapsed()?.as_secs()
+        ),
+        lines,
+    )?;
 
     Ok(())
 }
