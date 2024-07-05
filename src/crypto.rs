2f9b8819c2e0c178370df8bb26406f6c35feaa55
sgdxbc <sung@comp.nus.edu.sg>
2024-07-05 11:12:39 +0800

    Rectify crypto module

--- src/crypto.rs
+++ src/crypto.rs
@@ -2,6 +2,8 @@ use std::hash::{Hash, Hasher};
 
 use blake2::Blake2b;
 use derive_more::Deref;
+use derive_where::derive_where;
+use rand::{CryptoRng, RngCore};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
@@ -128,7 +130,7 @@ pub mod events {
     pub struct Verified<M, S = super::Signature>(pub super::Verifiable<M, S>);
 }
 
-// the cryptographic library must support seedable RNG based keypair generation
+// the cryptographic library must support seedable RNG based key pair generation
 // to be used in this codebase
 // it would be better if the library supports prehashed message as well, but a
 // fallback `impl DigestHasher for Vec<u8>` is provided above anyway
@@ -137,7 +139,28 @@ pub mod events {
 pub enum Signature {
     Plain(String), // for testing
     Secp256k1(secp256k1::ecdsa::Signature),
-    Schnorrkel(peer::Signature),
+    Schnorrkel(SchnorrkelSignature),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub struct SchnorrkelSignature(pub schnorrkel::Signature);
+
+impl Ord for SchnorrkelSignature {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.to_bytes().cmp(&other.0.to_bytes())
+    }
+}
+
+impl PartialOrd for SchnorrkelSignature {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Hash for SchnorrkelSignature {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        Hash::hash(&self.0.to_bytes(), state)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -150,13 +173,21 @@ pub struct Crypto {
 enum CryptoProvider {
     Insecure(String), // the "signature"
     Secp256k1(Secp256k1Crypto),
-    Schnorrkel(Box<peer::Crypto>),
+    Schnorrkel(Box<SchnorrkelCrypto>),
 }
 
 #[derive(Debug, Clone)]
 struct Secp256k1Crypto {
     secret_key: secp256k1::SecretKey,
     secp: secp256k1::Secp256k1<secp256k1::All>,
+}
+
+#[derive(Clone)]
+#[derive_where(Debug)]
+pub struct SchnorrkelCrypto {
+    pub keypair: schnorrkel::Keypair,
+    #[derive_where(skip)]
+    pub context: schnorrkel::context::SigningContext,
 }
 
 #[derive(Debug, Clone)]
@@ -221,7 +252,7 @@ impl Crypto {
                         .iter()
                         .map(|keypair| PublicKey::Schnorrkel(keypair.public))
                         .collect(),
-                    provider: CryptoProvider::Schnorrkel(Box::new(peer::Crypto {
+                    provider: CryptoProvider::Schnorrkel(Box::new(SchnorrkelCrypto {
                         keypair: secret_keys.remove(index.into()),
                         context: schnorrkel::signing_context(b"default"),
                     })),
@@ -246,14 +277,10 @@ impl Crypto {
                     ),
                 }
             }
-            CryptoProvider::Schnorrkel(crypto) => {
-                let signed = crypto.sign(message);
-                // this feels monkey patch = =
-                Verifiable {
-                    inner: signed.inner,
-                    signature: Signature::Schnorrkel(signed.signature),
-                }
-            }
+            CryptoProvider::Schnorrkel(crypto) => Verifiable {
+                signature: Signature::Schnorrkel(crypto.sign(&message)),
+                inner: message,
+            },
         }
     }
 
@@ -262,8 +289,9 @@ impl Crypto {
         index: impl Into<usize>,
         signed: &Verifiable<M>,
     ) -> anyhow::Result<()> {
-        let Some(public_key) = self.public_keys.get(index.into()) else {
-            anyhow::bail!("no identifier for index")
+        let index = index.into();
+        let Some(public_key) = self.public_keys.get(index) else {
+            anyhow::bail!("missing identifier for index {}", index)
         };
         match (&self.provider, public_key, &signed.signature) {
             (
@@ -280,18 +308,16 @@ impl Crypto {
                 let digest = secp256k1::Message::from_digest(signed.inner.sha256().into());
                 crypto.secp.verify_ecdsa(&digest, signature, public_key)?
             }
-            // this feels even more monkey patch > <
-            (
-                CryptoProvider::Schnorrkel(crypto),
-                PublicKey::Schnorrkel(public_key),
-                Signature::Schnorrkel(signature),
-            ) => crypto.verify_internal(public_key, &signed.inner, signature)?,
+            (CryptoProvider::Schnorrkel(crypto), PublicKey::Schnorrkel(public_key), _) => crypto
+                .verify(public_key, signed, |signature| match signature {
+                    Signature::Schnorrkel(signature) => Ok(signature),
+                    _ => anyhow::bail!("unimplemented"),
+                })?,
             _ => anyhow::bail!("unimplemented"),
         }
         Ok(())
     }
 
-    // TODO deduplicate with the `peer::Crypto` version
     pub fn verify_batch<I: Clone + Into<usize>, M: DigestHash>(
         &self,
         indexes: &[I],
@@ -300,64 +326,22 @@ impl Crypto {
         let CryptoProvider::Schnorrkel(crypto) = &self.provider else {
             anyhow::bail!("unimplemented") // TODO fallback to verify one by one?
         };
-        let mut transcripts = Vec::new();
-        let mut signatures = Vec::new();
-        let mut public_keys = Vec::new();
-        for (index, verifiable) in indexes.iter().zip(signed) {
-            let (
-                PublicKey::Schnorrkel(public_key),
-                Signature::Schnorrkel(peer::Signature(signature)),
-            ) = (
-                &self.public_keys[index.clone().into()],
-                &verifiable.signature,
-            )
-            else {
-                anyhow::bail!("unimplemented")
-            };
-            let mut state = Sha256::new();
-            DigestHash::hash(&verifiable.inner, &mut state);
-            transcripts.push(crypto.context.hash256(state));
-            signatures.push(*signature);
-            public_keys.push(*public_key)
-        }
-        schnorrkel::verify_batch(transcripts, &signatures, &public_keys, true)
-            .map_err(anyhow::Error::msg)
+        let public_keys = indexes
+            .iter()
+            .map(|i| match &self.public_keys[i.clone().into()] {
+                PublicKey::Schnorrkel(key) => Ok(*key),
+                _ => anyhow::bail!("unimplemented"),
+            })
+            .collect::<anyhow::Result<Vec<_>>>()?;
+        crypto.verify_batch(&public_keys, signed, |signature| match signature {
+            Signature::Schnorrkel(signature) => Ok(signature),
+            _ => anyhow::bail!("unimplemented"),
+        })
     }
 }
 
 pub mod peer {
-    use std::{fmt::Debug, hash::Hash};
-
-    use derive_where::derive_where;
-    use rand::{CryptoRng, RngCore};
-    use schnorrkel::{context::SigningContext, Keypair};
-    use serde::{Deserialize, Serialize};
-    use sha2::{Digest, Sha256};
-
-    use super::DigestHash;
-
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-    pub struct Signature(pub schnorrkel::Signature);
-
-    impl Ord for Signature {
-        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-            self.0.to_bytes().cmp(&other.0.to_bytes())
-        }
-    }
-
-    impl PartialOrd for Signature {
-        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
-    impl Hash for Signature {
-        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-            Hash::hash(&self.0.to_bytes(), state)
-        }
-    }
-
-    pub type Verifiable<M> = super::Verifiable<M, Signature>;
+    pub type Verifiable<M> = super::Verifiable<M, super::SchnorrkelSignature>;
 
     pub type PublicKey = schnorrkel::PublicKey;
 
@@ -368,74 +352,57 @@ pub mod peer {
         #[derive(Debug, Clone)]
         pub struct Verified<M>(pub super::Verifiable<M>);
     }
+}
 
-    #[derive(Clone)]
-    #[derive_where(Debug)]
-    pub struct Crypto {
-        pub keypair: Keypair,
-        #[derive_where(skip)]
-        pub context: SigningContext,
+impl SchnorrkelCrypto {
+    pub fn new_random(rng: &mut (impl RngCore + CryptoRng)) -> Self {
+        Self {
+            keypair: schnorrkel::Keypair::generate_with(rng),
+            context: schnorrkel::context::SigningContext::new(b"default"),
+        }
     }
 
-    impl Crypto {
-        pub fn new_random(rng: &mut (impl CryptoRng + RngCore)) -> Self {
-            Self {
-                keypair: Keypair::generate_with(rng),
-                context: SigningContext::new(b"default"),
-            }
-        }
+    pub fn public_key(&self) -> schnorrkel::PublicKey {
+        self.keypair.public
+    }
 
-        pub fn public_key(&self) -> PublicKey {
-            self.keypair.public
-        }
+    pub fn sign<M: DigestHash>(&self, message: &M) -> SchnorrkelSignature {
+        let mut state = Sha256::new();
+        DigestHash::hash(message, &mut state);
+        SchnorrkelSignature(self.keypair.sign(self.context.hash256(state)))
+    }
 
-        pub fn sign<M: DigestHash>(&self, message: M) -> Verifiable<M> {
+    pub fn verify<M: DigestHash, S>(
+        &self,
+        public_key: &schnorrkel::PublicKey,
+        signed: &Verifiable<M, S>,
+        as_signature: impl FnOnce(&S) -> anyhow::Result<&SchnorrkelSignature>,
+    ) -> anyhow::Result<()> {
+        let SchnorrkelSignature(signature) = as_signature(&signed.signature)?;
+        let mut state = Sha256::new();
+        DigestHash::hash(&signed.inner, &mut state);
+        public_key
+            .verify(self.context.hash256(state), signature)
+            .map_err(anyhow::Error::msg)
+    }
+
+    pub fn verify_batch<M: DigestHash, S>(
+        &self,
+        public_keys: &[schnorrkel::PublicKey],
+        signed: &[Verifiable<M, S>],
+        mut as_signature: impl FnMut(&S) -> anyhow::Result<&SchnorrkelSignature>,
+    ) -> anyhow::Result<()> {
+        let mut transcripts = Vec::new();
+        let mut signatures = Vec::new();
+        for verifiable in signed {
             let mut state = Sha256::new();
-            DigestHash::hash(&message, &mut state);
-            let signature = self.keypair.sign(self.context.hash256(state));
-            Verifiable {
-                inner: message,
-                signature: Signature(signature),
-            }
+            DigestHash::hash(&verifiable.inner, &mut state);
+            transcripts.push(self.context.hash256(state));
+            let SchnorrkelSignature(signature) = as_signature(&verifiable.signature)?;
+            signatures.push(*signature);
         }
-
-        pub fn verify<M: DigestHash>(
-            &self,
-            public_key: &PublicKey,
-            signed: &Verifiable<M>,
-        ) -> anyhow::Result<()> {
-            self.verify_internal(public_key, &signed.inner, &signed.signature)
-        }
-
-        pub fn verify_internal<M: DigestHash>(
-            &self,
-            public_key: &PublicKey,
-            message: &M,
-            Signature(signature): &Signature,
-        ) -> anyhow::Result<()> {
-            let mut state = Sha256::new();
-            DigestHash::hash(message, &mut state);
-            public_key
-                .verify(self.context.hash256(state), signature)
-                .map_err(anyhow::Error::msg)
-        }
-
-        pub fn verify_batch<M: DigestHash>(
-            &self,
-            public_keys: &[PublicKey],
-            signed: &[Verifiable<M>],
-        ) -> anyhow::Result<()> {
-            let mut transcripts = Vec::new();
-            let mut signatures = Vec::new();
-            for verifiable in signed {
-                let mut state = Sha256::new();
-                DigestHash::hash(&verifiable.inner, &mut state);
-                transcripts.push(self.context.hash256(state));
-                signatures.push(verifiable.signature.0);
-            }
-            schnorrkel::verify_batch(transcripts, &signatures, public_keys, true)
-                .map_err(anyhow::Error::msg)
-        }
+        schnorrkel::verify_batch(transcripts, &signatures, public_keys, true)
+            .map_err(anyhow::Error::msg)
     }
 }
 
@@ -470,6 +437,3 @@ mod tests {
         crypto[0].verify_batch(&[0usize, 1, 2, 3], &verifiable)
     }
 }
-
-// cSpell:words hasher Borsh endianness seedable keypair prehashed secp256k1
-// cSpell:words schnorrkel secp
